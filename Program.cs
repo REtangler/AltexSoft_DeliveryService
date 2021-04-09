01242eb3f6c695ec9ae1519c928ec30b0e4e7eb2
REtangler <63062068+REtangler@users.noreply.github.com>
2021-04-09 23:40:14 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AltexSoft_DeliveryService
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
