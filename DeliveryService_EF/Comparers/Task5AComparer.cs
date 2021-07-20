7198b044b46450892468e8e58062ff59bd81b86d
REtangler <63062068+REtangler@users.noreply.github.com>
2021-07-20 04:06:15 +0300

    Created LINQ requests to satisfy task requirements

---
+++ DeliveryService_EF/Comparers/Task5AComparer.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using DeliveryService_EF.Models;
+
+namespace DeliveryService_EF.Comparers
+{
+    public class Task5AComparer : IEqualityComparer<Product>
+    {
+        public bool Equals(Product x, Product y)
+        {
+            return x.Name.Equals(y.Name);
+        }
+
+        public int GetHashCode(Product obj)
+        {
+            return obj.Name.GetHashCode();
+        }
+    }
+}
--- DeliveryService_EF/Models/Product.cs
+++ DeliveryService_EF/Models/Product.cs
@@ -1,4 +1,6 @@
-﻿namespace DeliveryService_EF.Models
+﻿using System.Collections.Generic;
+
+namespace DeliveryService_EF.Models
 {
     public class Product
     {
@@ -8,6 +10,93 @@
         public decimal Price { get; set; }
         public int AmountInStock { get; set; }
         public Category CategoryId { get; set; }
-        public Supplier SupplierId { set; get; }
+        public Supplier SupplierId { get; set; }
+
+        public static IList<Product> GetProducts()
+        {
+            return new List<Product>
+            {
+                new Product
+                {
+                    Id = 1,
+                    Name = "Strix RTX 2070",
+                    Description = "Nvidia GPU capable of ray-tracing",
+                    Price = 21250,
+                    AmountInStock = 4,
+                    CategoryId = Category.GetCategory(1),
+                    SupplierId = Supplier.GetSupplier(2)
+                },
+                new Product
+                {
+                    Id = 2,
+                    Name = "Strix RTX 2070",
+                    Description = "Nvidia GPU capable of ray-tracing",
+                    Price = 21250,
+                    AmountInStock = 4,
+                    CategoryId = Category.GetCategory(1),
+                    SupplierId = Supplier.GetSupplier(1)
+                },
+                new Product
+                {
+                    Id = 3,
+                    Name = "Radeon RX 470",
+                    Description = "AMD GPU",
+                    Price = 18050,
+                    AmountInStock = 13,
+                    CategoryId = Category.GetCategory(1),
+                    SupplierId = Supplier.GetSupplier(1)
+                },
+                new Product
+                {
+                    Id = 4,
+                    Name = "Intel i7-10850K",
+                    Description = "Intel multi-threaded CPU",
+                    Price = 5099,
+                    AmountInStock = 45,
+                    CategoryId = Category.GetCategory(2),
+                    SupplierId = Supplier.GetSupplier(1)
+                },
+                new Product
+                {
+                    Id = 5,
+                    Name = "Intel i7-10850K",
+                    Description = "Intel multi-threaded CPU",
+                    Price = 5099,
+                    AmountInStock = 45,
+                    CategoryId = Category.GetCategory(2),
+                    SupplierId = Supplier.GetSupplier(2)
+                },
+                new Product
+                {
+                    Id = 6,
+                    Name = "Ryzen 5040",
+                    Description = "AMD performance oriented CPU",
+                    Price = 4749,
+                    AmountInStock = 67,
+                    CategoryId = Category.GetCategory(2),
+                    SupplierId = Supplier.GetSupplier(1)
+                },
+                new Product
+                {
+                    Id = 7,
+                    Name = "ASRock X570",
+                    Description = "ASRock motherboard targeted at creators",
+                    Price = 7199,
+                    AmountInStock = 102,
+                    CategoryId = Category.GetCategory(3),
+                    SupplierId = Supplier.GetSupplier(1)
+                },
+                new Product
+                {
+                    Id = 8,
+                    Name = "Asus Pro Q470M",
+                    Description = "Asus cutting-edge motherboard",
+                    Price = 8499,
+                    AmountInStock = 81,
+                    CategoryId = Category.GetCategory(3),
+                    SupplierId = Supplier.GetSupplier(2)
+                }
+            };
+        }
     }
 }
