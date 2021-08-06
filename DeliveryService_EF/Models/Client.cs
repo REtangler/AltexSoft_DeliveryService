9dd4a36c2abaf25af897e748bba332123c6a8f0d
REtangler <63062068+REtangler@users.noreply.github.com>
2021-08-07 01:45:07 +0300

    First half of HW (excluding IRepository & IUnitOfWork)

--- DeliveryService_EF/Models/Client.cs
+++ DeliveryService_EF/Models/Client.cs
@@ -1,10 +1,15 @@
-﻿namespace DeliveryService_EF.Models
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DeliveryService_EF.Models
 {
     public class Client
     {
         public int Id { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
+
+        [NotMapped]
+        public string FullName => FirstName + " " + LastName;
         public string PhoneNumber { get; set; }
         public string Address { get; set; }
         public string Email { get; set; }
